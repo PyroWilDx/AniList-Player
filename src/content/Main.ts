7778207aedc94f2a89f438d7de6fd441757aa605
PyroWilDx <pyrowildx314.kqm@gmail.com>
2025-03-23 12:25:58 +0100

    PlayButton Styling

--- src/content/Main.ts
+++ src/content/Main.ts
@@ -3,6 +3,15 @@ import AniListScraper from "./scraper/AniListScraper";
 
 console.log("AniList-Player Started.");
 
+const styleTag = document.createElement("style");
+styleTag.textContent = `
+                @keyframes spin-anilist-player {
+                    0% { transform: rotate(0deg); }
+                    100% { transform: rotate(360deg); }
+                }
+            `;
+document.head.appendChild(styleTag);
+
 AniListScraper.WatchEntryRow((entryRow) => {
     AniListPlayer.GeneratePlayButton(entryRow);
 });
--- src/content/player/AniListPlayer.ts
+++ src/content/player/AniListPlayer.ts
@@ -17,14 +17,47 @@ export default class AniListPlayer {
         }
 
         const playButton = document.createElement("button");
+        playButton.style.cursor = "pointer";
+        playButton.style.display = "flex";
+        playButton.style.alignItems = "center";
+        playButton.style.justifyContent = "center";
+        playButton.style.padding = "8px 16px";
         playButton.textContent = "Play";
+        playButton.style.fontSize = "18px";
+        playButton.style.fontWeight = "bold";
+        playButton.style.color = "white";
+        playButton.style.backgroundColor = "#FF0000";
+        playButton.style.border = "none";
+        playButton.style.borderRadius = "8px";
+        playButton.style.transition = "background 0.2s ease";
+        playButton.onmouseover = () => (playButton.style.backgroundColor = "#CC0000");
+        playButton.onmouseout = () => (playButton.style.backgroundColor = "#FF0000");
         playButton.addEventListener("click", async () => {
+            const playButtonSize = playButton.getBoundingClientRect();
+            playButton.textContent = "";
+            playButton.style.width = `${playButtonSize.width}px`;
+            playButton.style.height = `${playButtonSize.height}px`;
+
+            const loaderIcon = document.createElement("div");
+            loaderIcon.style.width = "24px";
+            loaderIcon.style.height = "24px";
+            loaderIcon.style.border = "3.2px solid white";
+            loaderIcon.style.borderTop = "3.2px solid transparent";
+            loaderIcon.style.borderRadius = "50%";
+            loaderIcon.style.animation = "spin-anilist-player 1s linear infinite";
+
+            playButton.appendChild(loaderIcon);
+
             const episodeNumber = AniListScraper.GetEpisodeNumber(entryRowChildren);
             if (!episodeNumber) {
                 return;
             }
 
             await AniListPlayer.PlayEpisode(aniListId, episodeNumber);
+
+            playButton.textContent = "Play";
+            playButton.style.width = "";
+            playButton.style.height = "";
         });
 
         entryRow.insertBefore(playButton, entryRowChildren.titleElement.nextSibling);
