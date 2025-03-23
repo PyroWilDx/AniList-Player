95d2f7c36838299c10a72cb0e000999acd2c0735
PyroWilDx <pyrowildx314.kqm@gmail.com>
2025-03-23 21:44:24 +0100

    Retrieve Saved Provider

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -1,4 +1,18 @@
 const providersOpt = document.getElementById("providers-opt");
-providersOpt.addEventListener("change", () => {
-    chrome.storage.sync.set({ provider: providersOpt.value }, () => {});
+
+chrome.storage.sync.get(["provider"], (result) => {
+    if (result.provider) {
+        providersOpt.value = result.provider;
+    } else {
+        providersOpt.value = "AnimEmbed";
+        StoreProvider("AnimEmbed");
+    }
 });
+
+providersOpt.addEventListener("change", () => {
+    StoreProvider(providersOpt.value);
+});
+
+function StoreProvider(provider) {
+    chrome.storage.sync.set({ provider }, () => {});
+}
